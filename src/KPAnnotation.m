67aa734cf427e6614bce3c70fd5c31d525da1ef1
Stanislaw Pankevich <s.pankevich@gmail.com>
2014-03-10 20:41:26 +0400

    1.2-1.5x faster -[KPAnnotation calculateValues]

--- src/KPAnnotation.m
+++ src/KPAnnotation.m
@@ -49,30 +49,89 @@
 #pragma mark - Private
 
 - (void)calculateValues {
-    
-    CLLocationDegrees minLat = INT_MAX;
-    CLLocationDegrees minLng = INT_MAX;
-    CLLocationDegrees maxLat = -INT_MAX;
-    CLLocationDegrees maxLng = -INT_MAX;
-    
+    NSUInteger count = self.annotations.count;
+
+    if (count == 0) {
+        return;
+    }
+
+    CLLocationDegrees minLat = NSIntegerMax;
+    CLLocationDegrees minLng = NSIntegerMax;
+    CLLocationDegrees maxLat = NSIntegerMin;
+    CLLocationDegrees maxLng = NSIntegerMin;
+
     CLLocationDegrees totalLat = 0;
     CLLocationDegrees totalLng = 0;
-    
-    for(id<MKAnnotation> a in self.annotations){
-        
-        CLLocationDegrees lat = [a coordinate].latitude;
-        CLLocationDegrees lng = [a coordinate].longitude;
-        
-        minLat = MIN(minLat, lat);
-        minLng = MIN(minLng, lng);
-        maxLat = MAX(maxLat, lat);
-        maxLng = MAX(maxLng, lng);
-        
-        totalLat += lat;
-        totalLng += lng;
+
+    NSUInteger idx = 0;
+    CLLocationCoordinate2D coords[2];
+
+    /* 
+     This algorithm is approx. 1.2-2x faster than naive one.
+     It is described here: https://github.com/EvgenyKarkan/EKAlgorithms/issues/30
+     */
+    for (id <MKAnnotation> ithAnnotation in self.annotations) {
+        // Machine way of doing odd/even check is better than mathematical count % 2
+        if (((idx++) & 1) == 0) {
+            coords[0] = ithAnnotation.coordinate;
+
+            continue;
+        } else {
+            coords[1] = ithAnnotation.coordinate;
+        }
+
+        CLLocationDegrees ithLatitude      = coords[0].latitude;
+        CLLocationDegrees iplus1thLatitude = coords[1].latitude;
+
+        CLLocationDegrees ithLongitude      = coords[0].longitude;
+        CLLocationDegrees iplus1thLongitude = coords[1].longitude;
+
+        if (ithLatitude < iplus1thLatitude) {
+            minLat = MIN(minLat, ithLatitude);
+            maxLat = MAX(maxLat, iplus1thLatitude);
+        }
+        else if (ithLatitude > iplus1thLatitude) {
+            minLat = MIN(minLat, iplus1thLatitude);
+            maxLat = MAX(maxLat, ithLatitude);
+        }
+        else {
+            minLat = MIN(minLat, ithLatitude);
+            maxLat = MAX(maxLat, ithLatitude);
+        }
+
+        if (ithLongitude < iplus1thLongitude) {
+            minLng = MIN(minLng, ithLongitude);
+            maxLng = MAX(maxLng, iplus1thLongitude);
+        }
+        else if (ithLongitude > iplus1thLongitude) {
+            minLng = MIN(minLng, iplus1thLongitude);
+            maxLng = MAX(maxLng, ithLongitude);
+        }
+        else {
+            minLng = MIN(minLng, ithLongitude);
+            maxLng = MAX(maxLng, ithLongitude);
+        }
+
+        totalLat += (ithLatitude + iplus1thLatitude);
+        totalLng += (ithLongitude + iplus1thLongitude);
     }
-    
-    
+
+    // If self.annotations has odd number elements we have unpaired last annotation coordinate values in coords[0]
+    BOOL isOdd = count & 1;
+
+    if (isOdd == 1) {
+        CLLocationDegrees lastElementLatitude  = coords[0].latitude;
+        CLLocationDegrees lastElementLongitude = coords[1].longitude;
+
+        minLat = MIN(minLat, lastElementLatitude);
+        minLng = MIN(minLng, lastElementLongitude);
+        maxLat = MAX(maxLat, lastElementLatitude);
+        maxLng = MAX(maxLng, lastElementLongitude);
+
+        totalLat += lastElementLatitude;
+        totalLng += lastElementLongitude;
+    }
+
     self.coordinate = CLLocationCoordinate2DMake(totalLat / self.annotations.count,
                                                  totalLng / self.annotations.count);
     
